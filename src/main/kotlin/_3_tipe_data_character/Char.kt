4fef3dbdaeda50c0199c910b174c0fc98d4ea3d7
Yogadimas <yogadimaspambudi@gmail.com>
2023-08-29 09:14:30 +0700

    Tipe Data Char (Karakter)

---
+++ src/main/kotlin/_3_tipe_data_character/Char.kt
@@ -0,0 +1,13 @@
+package _3_tipe_data_character
+
+fun main() {
+    var y: Char = 'Y'
+    var o: Char = 'O'
+    var g: Char = 'G'
+    var a: Char = '4'
+
+    print(y)
+    print(o)
+    print(g)
+    print(a)
+}
