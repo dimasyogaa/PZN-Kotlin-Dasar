c8edb83a26a34b18e5682bba900953c5a8bf995b
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 22:23:42 +0700

    Package Import

---
+++ src/main/kotlin/_38_Package_Import/PackageImport.kt
@@ -0,0 +1,8 @@
+package _38_Package_Import
+
+import _38_Package_Import.com.yogadimas.belajar.util.*
+
+fun main() {
+    sayHello("Yoga Dimas")
+    sayGoodBye("Yoga Dimas")
+}
---
+++ src/main/kotlin/_38_Package_Import/com/yogadimas/belajar/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package _38_Package_Import.com.yogadimas.belajar.util
+
+fun sayHello(name: String) {
+    println("Hello $name")
+}
+
+fun sayGoodBye(name: String) {
+    println("Goodbye $name")
+}
