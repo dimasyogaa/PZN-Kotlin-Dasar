e321899a00e030a83ea7ef6b52bb378e02439fc0
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 14:16:19 +0700

    Do While

---
+++ src/main/kotlin/_16_Do_While_Loop/DoWhileLoop.kt
@@ -0,0 +1,14 @@
+package _16_Do_While_Loop
+
+fun main() {
+
+    // minimal satu kali dilakukan perulangan
+
+    var i = 10
+
+    do {
+        println("Perulangan ke $i")
+        i++
+    } while (i < 10)
+
+}
