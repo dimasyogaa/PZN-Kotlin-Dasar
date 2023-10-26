85f0dac24055f9297e7ef6e610d9a0df1ff636cc
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 22:35:52 +0700

    Komentar

---
+++ src/main/kotlin/_40_Komentar/Komentar.kt
@@ -0,0 +1,23 @@
+package _40_Komentar
+
+/**
+ * Function untuk contoh
+ */
+fun contohFunction(){
+
+}
+
+/**
+ * Contoh function dengan parameter
+ * @param name nama parameter
+ */
+fun contohFunctionParameter(name: String){
+
+}
+
+fun main() {
+    // memanggil contoh function
+    contohFunction()
+    contohFunctionParameter("Eko") // memanggil contoh function parameter
+}
+
