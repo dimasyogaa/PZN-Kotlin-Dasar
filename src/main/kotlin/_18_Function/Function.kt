dd905e382a593cbb0e9c8aa539beda289980ee55
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 14:21:59 +0700

    Function

---
+++ src/main/kotlin/_18_Function/Function.kt
@@ -0,0 +1,12 @@
+package _18_Function
+
+fun helloWorld(){
+    println("Hello Dunia")
+    println("Selamat Belajar Kotlin")
+}
+
+fun main() {
+    helloWorld()
+    helloWorld()
+    helloWorld()
+}
