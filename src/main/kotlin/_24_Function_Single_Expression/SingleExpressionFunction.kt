5605f2d189d330814961fdfe392a76048f85c4ee
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 14:50:11 +0700

    Function - Single Expression

---
+++ src/main/kotlin/_24_Function_Single_Expression/SingleExpressionFunction.kt
@@ -0,0 +1,10 @@
+package _24_Function_Single_Expression
+
+fun kaliDua(a: Int): Int = a * 2
+
+fun sapa(name: String): Unit = println("Hello $name")
+
+fun main() {
+    println(kaliDua(10))
+    sapa("Yoga")
+}
