0d060edf1f673f94fe3580553b2c793c036e506d
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 14:34:56 +0700

    Function - Named Argument

---
+++ src/main/kotlin/_21_Function_Named_Argument/FunctionNamedArgument.kt
@@ -0,0 +1,18 @@
+package _21_Function_Named_Argument
+
+fun fullName(
+    firstName: String,
+    middleName: String,
+    lastName: String
+) {
+    println("Hello $firstName $middleName $lastName")
+}
+
+fun main() {
+    fullName("Yoga", "Dimas", "Pambudi")
+    fullName(
+        lastName = "Pambudi",
+        firstName = "Yoga",
+        middleName = "Dimas"
+    )
+}
