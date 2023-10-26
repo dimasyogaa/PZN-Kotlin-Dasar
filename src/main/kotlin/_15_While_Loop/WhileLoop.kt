e8d67bfb3e2bc24c492a1c21e17c0e6c8bffddae
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-26 14:14:57 +0700

    While Loop

---
+++ src/main/kotlin/_15_While_Loop/WhileLoop.kt
@@ -0,0 +1,16 @@
+package _15_While_Loop
+
+fun main() {
+
+    // while untuk perulangan dengan data bebas tidak hanya data iterator
+
+    var i = 0
+
+    while (i < 10){
+        println("Perulangan $i")
+        i++
+    }
+
+    println("Selesai perulangan")
+
+}
